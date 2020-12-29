936588f626e5e7db36eb841d719b9ffecee9e4be
a-bouts <arnaud.bouts@gmail.com>
2020-12-29 15:01:05 +0100

    fix when ice limit not provided

--- race/race.go
+++ race/race.go
@@ -83,17 +83,21 @@ func (iceLimits *IceLimits) IsInIceLimits(latLon *latlon.LatLon) bool {
 	}
 
 	if latLon.Lat > 0.0 {
-		i := int((lon + 180) / 5)
-		lat := (lon-iceLimits.North[i].Lon)/(iceLimits.North[i+1].Lon-iceLimits.North[i].Lon)*(iceLimits.North[i+1].Lat-iceLimits.North[i].Lat) + iceLimits.North[i].Lat
-		if latLon.Lat >= lat {
-			return true
+		if len(iceLimits.North) > 0 {
+			i := int((lon + 180) / 5)
+			lat := (lon-iceLimits.North[i].Lon)/(iceLimits.North[i+1].Lon-iceLimits.North[i].Lon)*(iceLimits.North[i+1].Lat-iceLimits.North[i].Lat) + iceLimits.North[i].Lat
+			if latLon.Lat >= lat {
+				return true
+			}
 		}
 		return false
 	} else {
-		i := int((lon + 180) / 5)
-		lat := (lon-iceLimits.South[i].Lon)/(iceLimits.South[i+1].Lon-iceLimits.South[i].Lon)*(iceLimits.South[i+1].Lat-iceLimits.South[i].Lat) + iceLimits.South[i].Lat
-		if latLon.Lat <= lat {
-			return true
+		if len(iceLimits.South) > 0 {
+			i := int((lon + 180) / 5)
+			lat := (lon-iceLimits.South[i].Lon)/(iceLimits.South[i+1].Lon-iceLimits.South[i].Lon)*(iceLimits.South[i+1].Lat-iceLimits.South[i].Lat) + iceLimits.South[i].Lat
+			if latLon.Lat <= lat {
+				return true
+			}
 		}
 		return false
 	}
